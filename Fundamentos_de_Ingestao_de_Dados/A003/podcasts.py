554dd79961456e10ab5ef261582220349bbf510b
Caroline Araujo <ca.araujosouza@gmail.com>
2023-10-28 15:51:24 -0300

    web scrapping no site podcast café brasil

--- Fundamentos_de_Ingestao_de_Dados/A003/podcasts.py
+++ Fundamentos_de_Ingestao_de_Dados/A003/podcasts.py
@@ -1,5 +1,6 @@
 #%%
 import requests
+from bs4 import BeautifulSoup as bs
 
 # %%
 url = 'https://portalcafebrasil.com.br/todos/podcasts/'
@@ -7,4 +8,20 @@ url = 'https://portalcafebrasil.com.br/todos/podcasts/'
 ret = requests.get(url)
 # %%
 ret.text
+# %%
+soup = bs(ret.text, features="lxml")
+
+# %%
+soup.find('h5')
+# %%
+soup.find('h5').text
+# %%
+soup.find('h5').a['href']
+
+# %%
+
+lst_podcasts = soup.find_all('h5')
+# %%
+for item in lst_podcasts:
+    print(f"EP: {item.text} - Link: {item.a['href']}")
 # %%
